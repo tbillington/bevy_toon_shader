d5c1644c087fcf8f48a8011dd9d11bd1c26d7604
Trent <2771466+tbillington@users.noreply.github.com>
2023-04-11 21:18:55 +0200

    init

---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+use bevy::{
+    asset::load_internal_asset,
+    pbr::{MaterialPipeline, MaterialPipelineKey},
+    prelude::*,
+    reflect::TypeUuid,
+    render::{
+        mesh::MeshVertexBufferLayout,
+        render_resource::{
+            AsBindGroup, AsBindGroupShaderType, RenderPipelineDescriptor, ShaderRef, ShaderType,
+            SpecializedMeshPipelineError,
+        },
+    },
+};
+
+pub const TOON_SHADER_HANDLE: HandleUntyped =
+    HandleUntyped::weak_from_u64(Shader::TYPE_UUID, 11079857277321826659);
+
+pub struct ToonShaderPlugin;
+
+impl Plugin for ToonShaderPlugin {
+    fn build(&self, app: &mut App) {
+        load_internal_asset!(
+            app,
+            TOON_SHADER_HANDLE,
+            "toon_shader.wgsl",
+            Shader::from_wgsl
+        );
+
+        app.add_plugin(MaterialPlugin::<ToonShaderMaterial>::default());
+    }
+}
+
+#[derive(AsBindGroup, TypeUuid, Debug, Clone)]
+#[uuid = "7b033895-875f-4cb5-97ae-8601fcc37053"]
+#[bind_group_data(ToonShaderMaterialKey)]
+#[uniform(0, ToonShaderMaterialUniform)]
+pub struct ToonShaderMaterial {
+    pub color: Color,
+    pub sun_pos: Vec3,
+    #[texture(1)]
+    #[sampler(2)]
+    pub base_color_texture: Option<Handle<Image>>,
+}
+
+impl Material for ToonShaderMaterial {
+    fn fragment_shader() -> ShaderRef {
+        TOON_SHADER_HANDLE.typed().into()
+    }
+
+    fn specialize(
+        _pipeline: &MaterialPipeline<Self>,
+        _descriptor: &mut RenderPipelineDescriptor,
+        _layout: &MeshVertexBufferLayout,
+        _key: MaterialPipelineKey<Self>,
+    ) -> Result<(), SpecializedMeshPipelineError> {
+        Ok(())
+    }
+}
+
+impl AsBindGroupShaderType<ToonShaderMaterialUniform> for ToonShaderMaterial {
+    fn as_bind_group_shader_type(
+        &self,
+        _images: &bevy::render::render_asset::RenderAssets<Image>,
+    ) -> ToonShaderMaterialUniform {
+        ToonShaderMaterialUniform {
+            color: self.color.into(),
+            sun_pos: self.sun_pos,
+        }
+    }
+}
+
+#[derive(Clone, Default, ShaderType)]
+pub struct ToonShaderMaterialUniform {
+    pub color: Vec4,
+    pub sun_pos: Vec3,
+}
+
+#[derive(Eq, PartialEq, Hash, Clone)]
+pub struct ToonShaderMaterialKey {}
+
+impl From<&ToonShaderMaterial> for ToonShaderMaterialKey {
+    fn from(_material: &ToonShaderMaterial) -> Self {
+        Self {}
+    }
+}
---
+++ src/toon_shader.wgsl
@@ -0,0 +1,27 @@
+struct ToonShaderMaterial {
+    color: vec4<f32>,
+    sun_pos: vec3<f32>,
+};
+
+@group(1) @binding(0)
+var<uniform> material: ToonShaderMaterial;
+@group(1) @binding(1)
+var base_color_texture: texture_2d<f32>;
+@group(1) @binding(2)
+var base_color_sampler: sampler;
+
+struct FragmentInput {
+    #import bevy_pbr::mesh_vertex_output
+}
+
+@fragment
+fn fragment (in: FragmentInput) -> @location(0) vec4<f32> {
+    let base_color = material.color * textureSample(base_color_texture, base_color_sampler, in.uv);
+    let normal = normalize(in.world_normal);
+    let n_dot_l = dot(material.sun_pos, normal);
+    var intensity: f32 = 0.0;
+    if n_dot_l > 0.0 {
+        intensity = 1.0;
+    }
+    return base_color * intensity;
+}
